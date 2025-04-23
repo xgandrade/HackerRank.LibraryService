e3cde00e8010aecf17c2a3bdaf1906f60c4a7f40
Guilherme Andrade <guilherme_gsa@live.com>
2025-04-23 15:47:58 -0300

    [feat] Base do projeto fornecida pelo HackerRank para desenvolvimento de API .NET

---
+++ LibraryService.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryService.WebAPI.Data;
+using LibraryService.WebAPI.Services;
+
+namespace LibraryService.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/libraries/{libraryId}/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILibrariesService _librariesService;
+        private readonly IBooksService _booksService;
+
+        public BooksController(IBooksService booksService, ILibrariesService librariesService)
+        {
+            _librariesService = librariesService;
+            _booksService = booksService;
+        }
+
+        // Implement the functionalities below
+    }
+}
